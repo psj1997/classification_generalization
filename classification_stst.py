06966a7d55ec8303b43c9ee105bef2ab6f130a9f
psj1997 <1176894699@qq.com>
2019-07-15 21:39:43 +0800

    update standard

--- classification_stst.py
+++ classification_stst.py
@@ -15,11 +15,11 @@ from warnings import simplefilter
 # ignore all future warnings
 
 Study = ['FR-CRC', 'AT-CRC', 'CN-CRC', 'US-CRC', 'DE-CRC']
-#Study = ['DE-CRC']
+#Study = ['AT-CRC']
 log_n0 = 1e-5
 sd_min_q = 0.1
 parameters_stst = {'FR-CRC':1000,'AT-CRC':1000,'CN-CRC':0.1,'US-CRC':1,'DE-CRC':100}
-parameters_loso = {'FR-CRC':0.1,'AT-CRC':0.1,'CN-CRC':0.1,'US-CRC':100,'DE-CRC':0.1}
+parameters_loso = {'FR-CRC':100,'AT-CRC':0.1,'CN-CRC':0.1,'US-CRC':0.1,'DE-CRC':0.1}
 
 if __name__ == '__main__':
 
@@ -35,9 +35,20 @@ if __name__ == '__main__':
     #predict_matrix store the prediction result
     predict_matrix = pd.DataFrame(np.zeros(shape=(species.shape[1],6)),index=sample_name,columns=['FR-CRC','AT-CRC','CN-CRC','US-CRC','DE-CRC','LOSO'])
 
+    accuracy_matrix_stst = {'FR-CRC': {'FR-CRC': [], 'AT-CRC': [], 'CN-CRC': [], 'US-CRC': [], 'DE-CRC': []},
+                       'AT-CRC': {'FR-CRC': [], 'AT-CRC': [], 'CN-CRC': [], 'US-CRC': [], 'DE-CRC': []},
+                       'CN-CRC': {'FR-CRC': [], 'AT-CRC': [], 'CN-CRC': [], 'US-CRC': [], 'DE-CRC': []},
+                       'US-CRC': {'FR-CRC': [], 'AT-CRC': [], 'CN-CRC': [], 'US-CRC': [], 'DE-CRC': []},
+                       'DE-CRC': {'FR-CRC': [], 'AT-CRC': [], 'CN-CRC': [], 'US-CRC': [], 'DE-CRC': []}}
+    accuracy_matrix_loso = {'FR-CRC': [], 'AT-CRC': [], 'CN-CRC': [], 'US-CRC': [], 'DE-CRC': []}
 
+
+
+    accuracy_matrix = pd.DataFrame(np.zeros(shape=(5,6)),index = ['FR-CRC', 'AT-CRC', 'CN-CRC', 'US-CRC', 'DE-CRC']
+                                   ,columns=['FR-CRC', 'AT-CRC', 'CN-CRC', 'US-CRC', 'DE-CRC','LOSO'])
     #train model for study to study transfer
     for study in Study:
+    #for study in ['AT-CRC']:
         print('training on {} with Study to Study Transfer'.format(study))
         # get sample_id about study
         train_data = pd.read_csv('pre_data//species_stst//{}//{}_data.csv'.format(study,study),header=0,index_col=0)
@@ -52,13 +63,13 @@ if __name__ == '__main__':
 
         # remove features with std=0
         x = x.values
-        std_preprocess = np.std(x,axis=0)
-        x = x[:,std_preprocess!=0]
-        x = np.log10(x + log_n0)
-        mean = np.mean(x, axis=0)
-        std = np.std(x, axis=0)
-        q = np.percentile(std,10)
-        x = (x - mean) / (std + q)
+        # std_preprocess = np.std(x,axis=0)
+        # x = x[:,std_preprocess!=0]
+        # x = np.log10(x + log_n0)
+        # mean = np.mean(x, axis=0)
+        # std = np.std(x, axis=0)
+        # q = np.percentile(std,10)
+        # x = (x - mean) / (std + q)
         y = y.values
         train_pre_matrix = pd.DataFrame(np.zeros(shape=(train_data.shape[0], 10)),
                                         index=train_sample_id,columns=['1', '2', '3','4', '5', '6', '7', '8', '9', '10'])
@@ -68,6 +79,7 @@ if __name__ == '__main__':
         # gs.fit(x, y)
         # print(gs.best_params_)
         #10 fold cross-validation repeated 10 times
+
         for i in range(10):
             skf = StratifiedKFold(n_splits=10)
             for train_index ,test_index in skf.split(x,y):
@@ -75,13 +87,27 @@ if __name__ == '__main__':
                 train_y = y[train_index]
                 test_x = x[test_index]
                 test_y = y[test_index]
+                #remove features with std=0
+                std_preprocess = np.std(train_x,axis=0)
+                train_x = train_x[:,std_preprocess!=0]
+                train_x = np.log10(train_x + log_n0)
+                mean = np.mean(train_x,axis=0)
+                std = np.std(train_x,axis=0)
+                q = np.percentile(std,10)
+                train_x = (train_x - mean) / (std + q)
                 lr = LogisticRegression(penalty='l1',solver='liblinear',n_jobs=-1,C=parameters_stst[study])
                 lr.fit(train_x,train_y)
                 #print(lr.n_iter_)
+                test_x = test_x[:,std_preprocess!=0]
+                test_x = np.log10(test_x + log_n0)
+                test_x = (test_x - mean) / (std + q)
                 proba = lr.predict_proba(test_x)[:,1]
                 train_sample_id = np.array(train_sample_id)
                 test_sample = train_sample_id[test_index]
                 train_pre_matrix.loc[test_sample,'%d'%(i+1)] = proba
+                score = lr.score(test_x,test_y)
+                #accuracy_matrix_stst[study][study].append(score)
+                accuracy_matrix.loc[study,study] += score / 100
                 for study_test in Study:
                     if study_test is study:
                         continue
@@ -97,6 +123,9 @@ if __name__ == '__main__':
                         test_stst_x = (test_stst_x - mean) / (std + q)
                         proba_stst = lr.predict_proba(test_stst_x)[:,1] / 100
                         predict_matrix.loc[test_stst_index,study] += proba_stst
+                        score = lr.score(test_stst_x,test_stst_y)
+                        #accuracy_matrix_stst[study][study_test].append(score)
+                        accuracy_matrix.loc[study,study_test] += score / 100
 
 
 
@@ -116,13 +145,13 @@ if __name__ == '__main__':
         # scalar.fit(x)
         # x = scalar.transform(x)
         x = x.values
-        std_preprocess = np.std(x, axis=0)
-        x = x[:, std_preprocess != 0]
-        x = np.log10(x + log_n0)
-        mean = np.mean(x, axis=0)
-        std = np.std(x, axis=0)
-        q = np.percentile(std,10)
-        x = (x - mean) / (std + q)
+        # std_preprocess = np.std(x, axis=0)
+        # x = x[:, std_preprocess != 0]
+        # x = np.log10(x + log_n0)
+        # mean = np.mean(x, axis=0)
+        # std = np.std(x, axis=0)
+        # q = np.percentile(std,10)
+        # x = (x - mean) / (std + q)
         y =y.values
         # parameters = {'C': [ 0.1, 0.5, 1, 5, 10, 100]}
         # gs = GridSearchCV(LogisticRegression(), parameters, refit=True, cv=10, verbose=1, n_jobs=-1)
@@ -134,13 +163,21 @@ if __name__ == '__main__':
             for train_index, test_index in skf.split(x, y):
                 train_x = x[train_index]
                 train_y = y[train_index]
+                std_preprocess = np.std(train_x, axis=0)
+                train_x = train_x[:, std_preprocess != 0]
+                train_x = np.log10(train_x + log_n0)
+                mean = np.mean(train_x, axis=0)
+                std = np.std(train_x, axis=0)
+                q = np.percentile(std, 10)
+                train_x = (train_x - mean) / (std + q)
                 test_x = x[test_index]
                 test_y = y[test_index]
-                lr = LogisticRegression(penalty='l1', solver='liblinear', n_jobs=-1,C = 0.1)
+                lr = LogisticRegression(penalty='l1', solver='liblinear', n_jobs=-1,C = parameters_loso[study])
                 lr.fit(train_x,train_y)
                 test_data = pd.read_csv('pre_data//species_loso//{}//test_data.csv'.format(study),header=0,index_col=0)
                 test_sample_id = test_data._stat_axis.values.tolist()
                 test_loso_y = test_data['Label']
+                test_loso_y = test_loso_y.values
                 test_loso_x = test_data.drop(columns='Label')
                 #test_loso_x = scalar.transform(test_loso_x)
                 test_loso_x = test_loso_x.values
@@ -149,11 +186,21 @@ if __name__ == '__main__':
                 test_loso_x = (test_loso_x - mean) / (std + q)
                 proba_loso = lr.predict_proba(test_loso_x)[:,1]/100
                 predict_matrix.loc[test_sample_id,'LOSO'] += proba_loso
+                score = lr.score(test_loso_x,test_loso_y)
+                #accuracy_matrix_loso[study].append(score)
+                accuracy_matrix.loc[study,'LOSO'] += score / 100
 
 
 
     #print(predict_matrix)
     predict_matrix.to_csv('predict_matrix.csv')
+    accuracy_matrix.to_csv('accuracy_matrix.csv')
+    # for study in Study:
+    #     for study1 in Study:
+    #         print('Training:{}  '.format(study),'Testing:{}  '.format(study1),'Accuracy:',np.array(accuracy_matrix_stst[study][study1]).mean())
+    #
+    # for study in Study:
+    #     print('Testing:{}  '.format(study),'Accuracy:',np.array(accuracy_matrix_loso[study]).mean())
 
 
 
