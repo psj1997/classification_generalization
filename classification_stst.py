d3f182a765954b1d2edcfb83eafad7ab70ede1bc
psj1997 <1176894699@qq.com>
2019-07-15 15:33:17 +0800

    update the norm and learning model

--- classification_stst.py
+++ classification_stst.py
@@ -8,10 +8,21 @@ from sklearn.model_selection import StratifiedKFold
 from sklearn.linear_model import Lasso
 from sklearn.linear_model import LogisticRegression
 import warnings
+from sklearn.model_selection import GridSearchCV
+
+# import warnings filter
+from warnings import simplefilter
+# ignore all future warnings
 
 Study = ['FR-CRC', 'AT-CRC', 'CN-CRC', 'US-CRC', 'DE-CRC']
+#Study = ['DE-CRC']
+log_n0 = 1e-5
+sd_min_q = 0.1
+parameters_stst = {'FR-CRC':1000,'AT-CRC':1000,'CN-CRC':0.1,'US-CRC':1,'DE-CRC':100}
+parameters_loso = {'FR-CRC':0.1,'AT-CRC':0.1,'CN-CRC':0.1,'US-CRC':100,'DE-CRC':0.1}
 
 if __name__ == '__main__':
+
     warnings.filterwarnings("ignore")
     # read .tsv file
     tag = 'species'
@@ -22,12 +33,11 @@ if __name__ == '__main__':
     #row name
     sample_name = meta['Sample_ID'].tolist()
     #predict_matrix store the prediction result
-    predict_matrix = pd.DataFrame(np.zeros(shape=(species.shape[1],5)),index=sample_name,columns=['FR-CRC','AT-CRC','CN-CRC','US-CRC','DE-CRC'])
+    predict_matrix = pd.DataFrame(np.zeros(shape=(species.shape[1],6)),index=sample_name,columns=['FR-CRC','AT-CRC','CN-CRC','US-CRC','DE-CRC','LOSO'])
 
 
-    # train model for study to study transfer
+    #train model for study to study transfer
     for study in Study:
-        #study = 'FR-CRC'
         print('training on {} with Study to Study Transfer'.format(study))
         # get sample_id about study
         train_data = pd.read_csv('pre_data//species_stst//{}//{}_data.csv'.format(study,study),header=0,index_col=0)
@@ -35,21 +45,39 @@ if __name__ == '__main__':
         y = train_data['Label']
         x = train_data.drop(columns='Label')
         # normalize using standard
-        x = StandardScaler().fit_transform(x)
-        train_pre_matrix = pd.DataFrame(np.zeros(shape=(train_data.shape[0], 10)), index=train_sample_id,columns=['1', '2', '3','4', '5', '6', '7', '8', '9', '10'])
+        # scalar = StandardScaler()
+        # scalar.fit(x)
+        # mean = scalar.mean_
+        # var = scalar.var_
 
+        # remove features with std=0
+        x = x.values
+        std_preprocess = np.std(x,axis=0)
+        x = x[:,std_preprocess!=0]
+        x = np.log10(x + log_n0)
+        mean = np.mean(x, axis=0)
+        std = np.std(x, axis=0)
+        q = np.percentile(std,10)
+        x = (x - mean) / (std + q)
+        y = y.values
+        train_pre_matrix = pd.DataFrame(np.zeros(shape=(train_data.shape[0], 10)),
+                                        index=train_sample_id,columns=['1', '2', '3','4', '5', '6', '7', '8', '9', '10'])
+
+        # parameters = {'C': [0.1, 0.5, 1, 5, 10, 100,1000]}
+        # gs = GridSearchCV(LogisticRegression(), parameters, refit=True, cv=10, n_jobs=-1)
+        # gs.fit(x, y)
+        # print(gs.best_params_)
         #10 fold cross-validation repeated 10 times
         for i in range(10):
             skf = StratifiedKFold(n_splits=10)
-            for train_index , test_index in skf.split(x,y):
-
+            for train_index ,test_index in skf.split(x,y):
                 train_x = x[train_index]
                 train_y = y[train_index]
                 test_x = x[test_index]
                 test_y = y[test_index]
-
-                lr = LogisticRegression(penalty='l1',solver='liblinear',n_jobs=-1)
+                lr = LogisticRegression(penalty='l1',solver='liblinear',n_jobs=-1,C=parameters_stst[study])
                 lr.fit(train_x,train_y)
+                #print(lr.n_iter_)
                 proba = lr.predict_proba(test_x)[:,1]
                 train_sample_id = np.array(train_sample_id)
                 test_sample = train_sample_id[test_index]
@@ -62,6 +90,11 @@ if __name__ == '__main__':
                         test_stst_index = test_stst._stat_axis.values.tolist()
                         test_stst_y = test_stst['Label']
                         test_stst_x = test_stst.drop(columns='Label')
+                        #test_stst_x = scalar.transform(test_stst_x)
+                        test_stst_x = test_stst_x.values
+                        test_stst_x = test_stst_x[:,std_preprocess!=0]
+                        test_stst_x = np.log10(test_stst_x+log_n0)
+                        test_stst_x = (test_stst_x - mean) / (std + q)
                         proba_stst = lr.predict_proba(test_stst_x)[:,1] / 100
                         predict_matrix.loc[test_stst_index,study] += proba_stst
 
@@ -69,11 +102,58 @@ if __name__ == '__main__':
 
 
 
-        train_pre_mean = train_pre_matrix.mean(1)
-        predict_matrix.loc[train_pre_matrix._stat_axis.values.tolist(),study] = train_pre_mean
+            train_pre_mean = train_pre_matrix.mean(1)
+            predict_matrix.loc[train_pre_matrix._stat_axis.values.tolist(),study] = train_pre_mean
 
-    print(predict_matrix)
-    predict_matrix.to_csv('predict_stst.csv')
+     # LOSO
+    for study in Study:
+        print('testing on study {} with LOSO'.format(study))
+        train_data = pd.read_csv('pre_data//species_loso//{}//train_data.csv'.format(study),header=0,index_col=0)
+        train_sample_id = train_data._stat_axis.values.tolist()
+        y = train_data['Label']
+        x = train_data.drop(columns='Label')
+        # scalar = StandardScaler()
+        # scalar.fit(x)
+        # x = scalar.transform(x)
+        x = x.values
+        std_preprocess = np.std(x, axis=0)
+        x = x[:, std_preprocess != 0]
+        x = np.log10(x + log_n0)
+        mean = np.mean(x, axis=0)
+        std = np.std(x, axis=0)
+        q = np.percentile(std,10)
+        x = (x - mean) / (std + q)
+        y =y.values
+        # parameters = {'C': [ 0.1, 0.5, 1, 5, 10, 100]}
+        # gs = GridSearchCV(LogisticRegression(), parameters, refit=True, cv=10, verbose=1, n_jobs=-1)
+        # gs.fit(x, y)
+        # print(gs.best_params_['C'])
+        for i in range(10):
+
+            skf = StratifiedKFold(n_splits=10)
+            for train_index, test_index in skf.split(x, y):
+                train_x = x[train_index]
+                train_y = y[train_index]
+                test_x = x[test_index]
+                test_y = y[test_index]
+                lr = LogisticRegression(penalty='l1', solver='liblinear', n_jobs=-1,C = 0.1)
+                lr.fit(train_x,train_y)
+                test_data = pd.read_csv('pre_data//species_loso//{}//test_data.csv'.format(study),header=0,index_col=0)
+                test_sample_id = test_data._stat_axis.values.tolist()
+                test_loso_y = test_data['Label']
+                test_loso_x = test_data.drop(columns='Label')
+                #test_loso_x = scalar.transform(test_loso_x)
+                test_loso_x = test_loso_x.values
+                test_loso_x = test_loso_x[:,std_preprocess != 0]
+                test_loso_x = np.log10(test_loso_x + log_n0)
+                test_loso_x = (test_loso_x - mean) / (std + q)
+                proba_loso = lr.predict_proba(test_loso_x)[:,1]/100
+                predict_matrix.loc[test_sample_id,'LOSO'] += proba_loso
+
+
+
+    #print(predict_matrix)
+    predict_matrix.to_csv('predict_matrix.csv')
 
 
 
